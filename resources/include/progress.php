3af1530d990425fbda5c1b07646e476179808b25
daredevil28 <seankelly@hotmail.nl>
2019-03-19 10:34:16 +0100

    Progress werkt nu weer goed. Als user gebruiker is kan deze alleen zijn eige progress zien

--- resources/include/progress.php
+++ resources/include/progress.php
@@ -1,6 +1,9 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
+$superadmin = false;
+
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 	header("location: index.php?content=home");
 	exit;
@@ -12,26 +15,26 @@ if($stmt = mysqli_prepare($conn, $sql)){
 	mysqli_stmt_store_result($stmt);
 	mysqli_stmt_bind_result($stmt, $role);
 }
-while (mysqli_stmt_fetch($stmt)) {
+if (mysqli_stmt_fetch($stmt)) {
 	if ($role == "gebruiker") {
-		echo "User is gebruiker";
+		$sql = "SELECT * FROM Users WHERE id = '$_SESSION[id]'";
+		$result = mysqli_query($conn, $sql);
 	}
 	if ($role == "admin") {
-		echo "User is admin";
+		$sql = "SELECT * FROM Users";
+		$result = mysqli_query($conn, $sql);
 	}
 	if ($role == "superadmin") {
-		echo "User is superadmin";
+		$sql = "SELECT * FROM Users";
+		$result = mysqli_query($conn, $sql);
+		$superadmin = true;
 	}
 }
 
-// $sql = "SELECT * FROM Users";
 
-// $result = mysqli_query($conn, $sql);
 
-// mysqli_close($conn);
-
-// $progress = "progress";
 ?>
+
 <main class="container">
 	<div class="row-auto">
 		<div class="col-auto">
@@ -43,24 +46,19 @@ while (mysqli_stmt_fetch($stmt)) {
 				<th scope="col">ID</th>
 				<th scope="col">Username</th>
 				<th scope="col">Progress</th>
+				<th scope="col">role</th>
 			</tr>
 		</thead>
 		<tbody>
-			<!--<?php
+			<?php
 			while ($record = mysqli_fetch_assoc($result)){
 				echo "<tr><th scope='row'>" . $record["id"] . "</th>
-				<td>" . $record["username"] . "</td>
+				<td>" . $record["email"] . "</td>
 				<td>" . $record["progress"] . "</td>
 				<td>" . $record["role"] . "</td>
-				<td>
-				<a href='./edit.php?id={$record['id']}'><img src='./resources/image/edit.png' width='30' alt='Edit record'</a>
-				</td>
-				<td>
-				<a href='./delet.php?id={$record['id']}'><img src='./resources/image/delete.png' width='30' alt='Delet record'</a>
-				</td>
 				</tr>";
 			}
-			?>-->
+			?>
 		</tbody>
 	</table>
 </main>
