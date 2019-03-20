0b65064aa00d61992a16e2bd1bd00a183bd4fccb
Dennis <dennisluijkenaar@gmail.com>
2019-03-20 12:25:59 +0100

    Tekst toegevoegd op pagina's

---
+++ resources/include/info.php
@@ -0,0 +1,31 @@
+<div class="info-content">
+    <h1>Informatie</h1>
+    <p>Je geheugen is een heel moeilijk concept. Het is namelijk het vermogen van het brein om bepaald gebeurtenissen
+    vast te houden. Maar hoe werkt je brein nou? Dat is een vraag wat de slimste wetenschappers bezig houd. Wel weten 
+    wij hoe je het brein aanpast, en dingen aanleert.</p>
+    <h3>Je brein kan je trainen</h3>
+    Je brein houdt gebeurtenissen vast als een soort van reflex. Je brein bestaat uit miljarden neuronen, en wanneer
+    deze op een bepaalde manier een stroompje versturen, komt een herrinering terug. Je neuronen onthouden deze 
+    herinnering in de vorm van impulsen. Deze neuronen kan je trainen door herhaaldelijk te oefenen met het onthouden 
+    van bijvoorbeeld patronen, volgordes, of foto's.
+    <h3>Wat is het verschil tussen lange- en korte termijngeheugen?</h3>
+    <p>Lange en korte termijn
+    Hoe werkt dat dan precies, het onthouden van informatie? Dat gebeurt in drie stappen. Je moet je voorstellen dat 
+    je via je omgeving allerlei sensorische informatie binnenkrijgt. Via je zintuigen komen die prikkels binnen. 
+    Geuren, stemmen, beelden, maar ook indrukken vanuit je lichaam zoals gevoelens. Kortom een chaotische wirwar aan 
+    prikkels.</p>
+    <p>Je sensorisch geheugen slaat al die indrukken heel even op. Maar dan wel voor maar drie tot vijf seconden. 
+    Je begrijpt dat het sensorische geheugen een grote capaciteit heeft. Al deze informatie kun je niet onthouden. 
+    Je richt je aandacht alleen op hetgene wat je belangrijk vindt. Je probeert bijvoorbeeld een telefoonnummer te 
+    onthouden wat je intoetst op je telefoon. Deze getallen komen in je kortetermijngeheugen (werkgeheugen). 
+    Gemiddeld kun je zeven dingen tegelijkertijd actief houden in je werkgeheugen. Zo kun je net lang genoeg het 
+    telefoonnummer onthouden om het te draaien. Maar meteen erna weet je het nummer al niet meer. Laat staan de 
+    volgende dag. Dit stukje geheugen gebruik je ook als je een ingewikkelde rekensom uit je hoofd probeert te doen. 
+    Hiermee onthoud je de tussenuitkomsten die je net hebt uitgerekend. Als je iets hebt onthouden in je 
+    kortetermijngeheugen komt het nog niet persé in je langetermijngeheugen terecht. Om informatie op te slaan in het 
+    langetermijngeheugen (referentiegeheugen) moet je er vaak wat meer aandacht aan besteden. Soms is het nodig om 
+    informatie vaker te herhalen of om het in verband te brengen met andere dingen die je al weet. Een pincode is 
+    makkelijker te onthouden als het je geboortedatum is. En je geboortedatum weet je al. Hierdoor komt het 
+    makkelijker in het langetermijngeheugen terecht. In dit geheugen sla je feiten en gebeurtenissen op 
+    (declaratief geheugen), maar ook hoe je iets moet doen (procedureel geheugen).</p>
+</div>
