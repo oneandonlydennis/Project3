c55323dc8b3443e6a4d811583b0e616c57857c3b
daredevil28 <seankelly@hotmail.nl>
2019-03-27 11:05:31 +0100

    Save game test 3

---
+++ resources/game/load.php
@@ -0,0 +1,12 @@
+<?php
+require_once "../../config.php";
+
+$sql = "SELECT id FROM users WHERE id = 7"; 
+
+if (mysqli_query($conn, $sql)) {
+	$record = mysqli_fetch_assoc($result);
+	echo $record["id"];
+} else {
+	echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+?>
