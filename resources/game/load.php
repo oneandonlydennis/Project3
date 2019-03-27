02ef2ff579f9dc2401ee87b76a537eb5d684f6a3
daredevil28 <seankelly@hotmail.nl>
2019-03-27 11:06:33 +0100

    Save game test 4

--- resources/game/load.php
+++ resources/game/load.php
@@ -1,7 +1,7 @@
 <?php
 require_once "../../config.php";
 
-$sql = "SELECT id FROM users WHERE id = 7"; 
+$sql = "SELECT id FROM Users WHERE id = 7"; 
 
 if (mysqli_query($conn, $sql)) {
 	$record = mysqli_fetch_assoc($result);
