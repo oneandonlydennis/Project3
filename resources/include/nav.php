47709578fdea8c7af5852b05558cef2dea5d3dbd
daredevil28 <seankelly@hotmail.nl>
2019-03-28 20:45:39 +0100

    More responsiveness

--- resources/include/nav.php
+++ resources/include/nav.php
@@ -7,9 +7,10 @@
 </style>
 <!-- Navigatie -->
 <nav class="navbar navbar-expand-md navbar-dark bg-primary">
-	<div class="logo"><a class="navbar-brand" href="./index.php?content=home"><img src="./resources/images/logo.png" alt="logo"></a></div>
+	<div class="logo"><a href="./index.php?content=home"><img src="./resources/images/logo.png" alt="logo"></a></div>
 	<button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-	aria-expanded="false" aria-label="Toggle navigation"></button>
+	aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+	
 	<div class="collapse navbar-collapse" id="collapsibleNavId">
 		<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
 			<li class="nav-item">
