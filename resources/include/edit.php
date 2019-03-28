f912666e1049966306f6521c14bdf058748e1a23
daredevil28 <seankelly@hotmail.nl>
2019-03-28 10:18:04 +0100

    edit comment

--- resources/include/edit.php
+++ resources/include/edit.php
@@ -1,18 +1,24 @@
 <?php
+#Is de persoon ingelogd?
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 	header("location: index.php?content=home");
+#Heeft de persoon "superadmin" als rol?
 } elseif ($_SESSION["role"] !== "superadmin") {
 	header("location: index.php");
 }
+#Als de button is ingedrukt
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+	#MYSQL injecties tegen gaan
 	$id = mysqli_real_escape_string($conn, $_POST["id"]);
 	$role = mysqli_real_escape_string($conn, $_POST["role"]);
 	$sql = "UPDATE Users SET role='$role' WHERE id='$id'";
+	#als het is gelukt stuur terug naar progress.php
 	if (mysqli_query($conn, $sql)) {
 		header("location: index.php?content=progress");
 	} else {
 		echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 	}
+	#als er geen button is ingedrukt, verkrijg de rol data van het te bewerken persoon
 } else {
 	$sql = "SELECT id, role FROM Users WHERE id = '$_GET[id]'";
 	$result = mysqli_query($conn, $sql);
@@ -33,6 +39,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 	<div class="form-group">
 		<input type="submit" class="btn btn-primary" value="Submit">
 	</div>
+	<!-- ID van de te bewerken persoon bewaren -->
 	<div class="form-group invisible">
       <input value="<?php echo $record['id']; ?>" name="id">
     </div>
