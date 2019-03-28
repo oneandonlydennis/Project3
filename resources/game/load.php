27dd132e9566dcaddd703a49490eda6b3341636e
daredevil28 <seankelly@hotmail.nl>
2019-03-28 10:50:52 +0100

    load comment

--- resources/game/load.php
+++ resources/game/load.php
@@ -1,10 +1,12 @@
 <?php
+//connectie database includen
 require_once "../../config.php";
 
 $sql = "SELECT progress FROM Users WHERE id = 7"; 
 
 if ($result = mysqli_query($conn, $sql)) {
 	$record = mysqli_fetch_assoc($result);
+	//laat de progress van de gebruiker zien
 	echo $record["progress"];
 } else {
 	echo "Error: " . $sql . "<br>" . mysqli_error($conn);
