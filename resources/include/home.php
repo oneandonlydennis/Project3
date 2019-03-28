ab365b3e6e9fe5cadc23fff315464af45b3299af
daredevil28 <seankelly@hotmail.nl>
2019-03-28 10:19:31 +0100

    home comment

--- resources/include/home.php
+++ resources/include/home.php
@@ -1,3 +1,4 @@
+<!-- home pagina waar mensen op komen -->
 <div class="home-content">
 	<h1>Welkom bij Geheugentraining.nl</h1>
 	<p>Op onze website kunt u verschillende info vinden over geheugentraining</p>
