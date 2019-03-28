f5d32eaf7784f6d2194d1814cdaac434bee57c7f
daredevil28 <seankelly@hotmail.nl>
2019-03-28 10:51:34 +0100

    save comment

--- resources/game/save.php
+++ resources/game/save.php
@@ -1,7 +1,9 @@
 <?php
+//connectie database includen
 require_once "../../config.php";
 $progress = mysqli_real_escape_string($conn, $_GET["progress"]); 
 
+//progress opslaan van gebruiker
 $sql = "UPDATE Users SET progress='$progress' WHERE id = 7"; 
 if (mysqli_query($conn, $sql)) {
 	echo "success";
