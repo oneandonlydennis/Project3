9c37e6449eb6d7138ee17a5837ec1d11a7970680
daredevil28 <seankelly@hotmail.nl>
2019-04-10 08:48:27 +0200

    Laatste game test

--- resources/game/save.php
+++ resources/game/save.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+header("Access-Control-Allow-Origin: *");
 //connectie database includen
 require_once "../../config.php";
 $progress = mysqli_real_escape_string($conn, $_GET["progress"]); 
